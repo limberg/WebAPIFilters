5a33e0915c7ed0e02df07e57b4b16b2e3362e5ae
Limberg Rivera <j_limberg@hotmail.com>
2020-12-16 22:24:50 +0100

    Basic Authentication Attribute

--- WebAPIFilters/Controllers/BasicAuthenticationController.cs
+++ WebAPIFilters/Controllers/BasicAuthenticationController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
 
 namespace WebAPIFilters.Controllers
 {
+    [BasicAuthentication.BasicAuthentication]
     public class BasicAuthenticationController : ApiController
     {
+       
+        [Route("api/basicauthentication")]
+        public string Get()
+        {
+            string usernameAuthenticated = Thread.CurrentPrincipal.Identity.Name;
+
+            return $"Authenticated Username: {usernameAuthenticated}";
+        }
     }
 }
---
+++ WebAPIFilters/LoginBasicAuthentication/LoginSecurity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIFilters.LoginBasicAuthentication
+{
+    public class LoginSecurity
+    {
+
+        public static bool Login(string username, string password)
+        {
+            //Case when Employye Tabelle existiert
+            //using(EmployeeDBEntities enitities = new EmployeeDBEntities())
+            //{
+            //    return entities.Users.Any(username => 
+            //        user.Username.Equals(username, StringComparison.OrdinalIgnoreCase)
+            //        && user.Pasword == password)
+            //    );
+            //}
+
+            return username.Equals("male", StringComparison.OrdinalIgnoreCase) && password == "male";
+        }
+    }
+}
