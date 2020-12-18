261af1acf3979c66febf6d241ce208cf9e40fcd7
Limberg Rivera <j_limberg@hotmail.com>
2020-12-18 18:02:21 +0100

    Simple OAuth Provider with Microsoft.Owin.Security.OAuth, Microsoft.AspNet.WebApi.Owin and Microsoft.AspNet.Identity.Owin

---
+++ WebAPIOAuthDemo/OAuthProvider/MyOAuthProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebAPIOAuthDemo.OAuthProvider
+{
+    public class MyOAuthProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var userIdentity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            //Read Credentitials from context and Validate wirh User DB
+
+            userIdentity.AddClaim(new Claim(ClaimTypes.Name, "Limberg"));
+            userIdentity.AddClaim(new Claim(ClaimTypes.Role, "Admin"));
+
+            context.Validated(userIdentity);
+
+            return;
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ WebAPIOAuthDemo/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using WebAPIOAuthDemo.OAuthProvider;
+
+[assembly: OwinStartup(typeof(WebAPIOAuthDemo.Startup))]
+
+namespace WebAPIOAuthDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            //2. Configure Options fpr UseOAuthAuthorizationServer
+            //OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions();
+            //options.AllowInsecureHttp = true;
+            //options.Provider = new MyOAuthProvider();
+
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(60),
+                TokenEndpointPath = new PathString("/GetToken"),
+                Provider = new MyOAuthProvider()
+            });
+
+            //3.
+
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            //1. Configurate Route for WebAPI
+
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                             name: "Default",
+                             routeTemplate: "api/{controller}"
+                         );
+
+            app.UseWebApi(config);
+        }
+    }
+}
